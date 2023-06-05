43fd2fdfdaadf4ba2e9d8135e56aa18a05ce2a83
arun <arun@openai.com>
2023-06-05 14:49:02 -0700

    test

--- .github/workflows/review-pr.py
+++ .github/workflows/review-pr.py
@@ -23,8 +23,23 @@ def reviewPR():
     repo = g.get_repo(f"{os.getenv('GITHUB_REPOSITORY')}")
     pr_number = os.getenv('GITHUB_REF').split('/')[-2]
     pr = repo.get_pull(int(pr_number))
+    # Get the list of files in the PR.
+    files = pr.get_files()
 
-    feedback = get_review_with_openai(pr.body)
+    # Initialize an empty string to store the contents of the files.
+    pr_content = ""
+
+    # For each file in the PR, get its content and append it to the pr_content string.
+    for file in files:
+        try:
+            file_content = repo.get_contents(file.filename, ref=pr.head.sha).decoded_content.decode()
+            pr_content += f"\n===={file.filename}====\n"
+            pr_content += file_content
+        except:
+            print(f"Error getting content of file {file.filename}")
+
+    # Generate feedback based on the contents of the files changed in the PR.
+    feedback = get_review_with_openai(pr_content)
 
     pr.create_review(body=feedback, event="COMMENT")
 
